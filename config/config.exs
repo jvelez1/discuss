1faa2c44b2b36465941c3365abbb1e5576875509
Josue Velez <josuevelezdr@gmail.com>
2019-06-23 14:10:19 +0200

    Add some basic authentication with github

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "2e92aa13885985b507ce",
+  client_secret: "ca4037aa4fa366e604959709176db4cd549cc5f8"
---
+++ web/models/user.ex
@@ -0,0 +1,18 @@
+defmodule Discuss.User do
+  use Discuss.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :provider, :string
+    field :token, :string
+
+    timestamps()
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:email, :provider, :token, :name])
+    |> validate_required([:email, :provider, :token])
+  end
+end
